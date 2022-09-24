28ba8b001e2d986750cf47db2ae7701dbb7a553a
AG <agentaidan@gmail.com>
2022-09-23 23:18:42 -0500

    page hero

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -26,54 +26,20 @@
 				class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
 			>
 				<li><a>Item 1</a></li>
-				<li tabindex="0">
-					<a class="justify-between">
-						Parent
-						<svg
-							class="fill-current"
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg
-						>
-					</a>
-					<ul class="p-2">
-						<li><a>Submenu 1</a></li>
-						<li><a>Submenu 2</a></li>
-					</ul>
-				</li>
 				<li><a>Item 3</a></li>
 			</ul>
 		</div>
-		<a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+		<a class="btn btn-ghost normal-case text-xl" href="/">UncRices</a>
 	</div>
 	<div class="navbar-center hidden lg:flex">
 		<ul class="menu menu-horizontal p-0">
 			<li><a>Item 1</a></li>
-			<li tabindex="0">
-				<a>
-					Parent
-					<svg
-						class="fill-current"
-						xmlns="http://www.w3.org/2000/svg"
-						width="20"
-						height="20"
-						viewBox="0 0 24 24"
-						><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg
-					>
-				</a>
-				<ul class="p-2">
-					<li><a>Submenu 1</a></li>
-					<li><a>Submenu 2</a></li>
-				</ul>
-			</li>
 			<li><a>Item 3</a></li>
 		</ul>
 	</div>
 	<div class="navbar-end">
         <ThemeChange />
-		<a class="btn">Get started</a>
+		<a class="btn btn-outline btn-accent" href="https://github.com/AidanG1/HackRice12" target="_blank">Source</a>
 	</div>
 </div>
 
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -7,6 +7,10 @@
         $question_num = question;
         $question_path = [...$question_path, question];
     };
+
+    const question_redirect = (url: string) => {
+        window.location.href = url;
+    };
 </script>
 
 <div class="flex">
@@ -17,7 +21,9 @@
         <div class="max-w-md">
             <h1 class="text-5xl font-bold">{flow[$question_num].question}</h1>
             {#each flow[$question_num].responses as response}
-                <button class="btn btn-primary btn-lg m-2" on:click={()=>{question_next(response.next)}}>{response.text}</button>
+                <button class="btn btn-primary btn-lg m-2" on:click={
+                ()=>{'next' in response ? question_next(response.next) : question_redirect(response.page)}
+                }>{response.text}</button>
             {/each}
         </div>
     </div>
