ed285b616a46490d9a70ef88ea1cffc8412099ed
AG <agentaidan@gmail.com>
2022-09-23 23:03:28 -0500

    create medical attention

---
+++ frontend/src/lib/Components/PageHero.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let hero_text: string = '';
+</script>
+
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">{hero_text}</h1>
+        <slot />
+      </div>
+    </div>
+  </div>
---
+++ frontend/src/routes/medical_attention/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import PageHero from '$lib/Components/PageHero.svelte';
+</script>
+
+<PageHero hero_text="Medical Attention" />
