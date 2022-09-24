057e551a4dbd8fc409e4134c56eb955b4f710659
AG <agentaidan@gmail.com>
2022-09-24 11:26:12 -0500

    enter send

--- frontend/src/routes/UserSend.svelte
+++ frontend/src/routes/UserSend.svelte
@@ -4,12 +4,13 @@
     let message: string = '';
 </script>
 
+<form on:submit|preventDefault={()=>{
+    send_user_message(message);
+    message = '';}}>
 <div class="border-t-secondary border-t content-center">
-    <div class="mt-1 w-full flex justify-center ">
-    <input type="text" bind:value={message} placeholder="Type a message..." class="input input-bordered input-primary w-5/6" />
-    <button class="btn btn-primary ml-2" on:click={() => {
-        send_user_message(message);
-        message = '';
-    }}>Send</button>
+        <div class="mt-1 w-full flex justify-center ">
+            <input type="text" bind:value={message} placeholder="Type a message..." class="input input-bordered input-primary w-5/6" />
+            <button class="btn btn-primary ml-2" type="submit">Send</button>
+        </div>
     </div>
-</div>
+</form>
