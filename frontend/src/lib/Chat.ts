8b2231ffbadde43f9bc6d59efa07d4343d0132da
Shreyas Minocha <sm-git@riseup.net>
2022-09-24 01:53:06 -0500

    Add supabase boilerplate

---
+++ frontend/src/lib/Chat.ts
@@ -0,0 +1,16 @@
+import {createClient} from '@supabase/supabase-js';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const SUPABASE_URL = process.env.SUPABASE_URL || '';
+const SUPABASE_PUBLIC_ANON_KEY = process.env.SUPABASE_PUBLIC_ANON_KEY || '';
+
+const supabase = createClient(
+	SUPABASE_URL,
+	SUPABASE_PUBLIC_ANON_KEY,
+);
+
+const { data, error } = await supabase.from('users').insert([{}]);
+
+console.log(data, error);
