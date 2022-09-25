472eef9492e58254b9ce5cc8e46cc445db8bf3e2
Shreyas Minocha <sm-git@riseup.net>
2022-09-25 06:01:31 -0500

    Add some input text field padding

--- frontend/src/routes/UserSend.svelte
+++ frontend/src/routes/UserSend.svelte
@@ -8,7 +8,7 @@
     send_user_message(message);
     message = '';}}>
 <div class="border-t-secondary border-t content-center">
-        <div class="mt-1 w-full flex justify-center ">
+        <div class="mt-1 py-4 w-full flex justify-center ">
             <input type="text" bind:value={message} placeholder="Type a message..." class="input input-bordered input-primary w-5/6" />
             <button class="btn btn-primary ml-2" type="submit">Send</button>
         </div>
